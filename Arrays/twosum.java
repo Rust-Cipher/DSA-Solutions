41eb4af3ace7d368525e13cb271a7bf8ed1d5b86
Rust-Cipher <sggamer1267@gmail.com>
2026-08-08 23:32:24 +0530

    Add Two Sum Solution

---
+++ Arrays/twosum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length -1 ;
+        int [] indexes = new int[2];
+
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            
+            if(sum == target){
+                indexes[0] = left;
+                indexes[1] = right;
+            }else if( sum < target){
+                left ++;
+
+            }else if(sum > target ){
+                right --;
+            }
+        }
+        return indexes;
+    }
+}
