a5c63c625f0710f8d3ee7859889430a793d68d2c
Rust-Cipher <sggamer1267@gmail.com>
2026-08-08 23:40:54 +0530

    Add Reverse A Number Solution

---
+++ Arrays/reversenumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+     long reverse = 0;
+        while(x != 0){
+            int remainder = x % 10;
+            reverse = (reverse * 10) + remainder;
+            x = x/10;
+        }
+        if(reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE){
+            return 0;
+        }else{
+            int reversedNumber = (int) reverse;
+            return reversedNumber;
+        }
+        
+        
+    }
+}
